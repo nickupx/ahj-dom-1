63de0d5ea53807d027c932ec855e538f9f22b59f
nickupx <nick.upsky@gmail.com>
2020-11-23 23:19:30 +0200

    improvements

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,5 +1,4 @@
 const result = '1'
-
 test('should be 1', () => {
   expect(result).toEqual('1')
 })
--- src/js/app.js
+++ src/js/app.js
@@ -5,36 +5,37 @@
 
 import '../css/style.css'
 
-// сделаем настраиваемый размер поля, но со склеиванием чисел и строк возиться лень, чтоб совсем динамически, поэтому ширины захардкодим
+// сделаем настраиваемый размер поля
 const FIELD_DIMENSION = 4
-const CELL_SIZE = '200px'
+const CELL_SIZE = 200
+
 const field = document.getElementById('field')
-field.style.width = '800px'
+field.style.width = `${FIELD_DIMENSION * CELL_SIZE}px`
 
 const imgSrc = 'https://github.com/netology-code/ahj-homeworks/raw/simplification/dom/pic/goblin.png'
 const img = document.createElement('img')
 img.src = imgSrc
 img.id = 'character'
 
-// рисуем поле
-for (let i = 1; i <= FIELD_DIMENSION * FIELD_DIMENSION; i++) {
+// нарисуем поле
+for (let i = 1; i <= FIELD_DIMENSION ** 2; i++) {
   const cell = document.createElement('div')
   cell.className = 'cell'
   cell.id = i
-  cell.style.width = CELL_SIZE
-  cell.style.height = CELL_SIZE
+  cell.style.width = `${CELL_SIZE}px`
+  cell.style.height = `${CELL_SIZE}px`
   field.appendChild(cell)
 }
 
-// подключаем генератор
-function getRandomIntInclusive(min, max) {
+// подключим генератор
+export default function getRandomIntInclusive(min, max) {
   min = Math.ceil(min)
   max = Math.floor(max)
   const result = Math.floor(Math.random() * (max - min + 1)) + min
   return result.toString()
 }
 
-document.getElementById(getRandomIntInclusive(1, FIELD_DIMENSION * FIELD_DIMENSION)).appendChild(img)
+document.getElementById(getRandomIntInclusive(1, FIELD_DIMENSION ** 2)).appendChild(img)
 
 function moveCharacter() {
   const prevPosition = document.getElementById('character').parentElement.id
